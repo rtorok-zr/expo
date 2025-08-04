lowRISC/opentitan

[base] Fill in random order hardening primitive.
Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

sw/device/lib/base/random_order.c
@@ -5,15 +5,62 @@
 #include "sw/device/lib/base/random_order.h"
 
 #include "sw/device/lib/base/bitfield.h"
+#include "sw/device/lib/base/hardened.h"
 
-// TODO: The current implementation is just a skeleton, and currently just
-// traverses from 0 to `min_len * 2`.
+void random_order_init(uint32_t (*rnd)(void), random_order_t *ctx,
+                       size_t min_len) {
+  // Position relative to start index is always initialized to zero.
+  ctx->pos = 0;
 
-void random_order_init(random_order_t *ctx, size_t min_len) {
-  ctx->state = 0;
-  ctx->max = min_len * 2;
+  // If the sequence length is zero or one, there's not much randomization we
+  // can do, but we can at least return one value past the range.
+  if (launder32(min_len) < 2) {
+    ctx->state = 0;
+    ctx->step = 1;
+    ctx->max = min_len + 1;
+    return;
+  }
+  HARDENED_CHECK_LE(2, min_len);
+
+  // Sample the start index from the range [0, n-1].
+  ctx->state = rnd() % min_len;
+  // Sample the step size from the range [1, n-1].
+  ctx->step = (rnd() % (min_len - 1)) + 1;
+
+  if (min_len - (min_len % ctx->step) > UINT32_MAX - ctx->step) {
+    // Safe fallback for cases where the max calculation would overflow.
+    ctx->max = UINT32_MAX;
+    ctx->step = 1;
+  } else {
+    // The maximum value is the smallest multiple of `step` that is strictly
+    // greater than `min_len`.
+    ctx->max = min_len - (min_len % ctx->step) + ctx->step;
+  }
+  HARDENED_CHECK_LE(min_len, ctx->max);
 }
 
 size_t random_order_len(const random_order_t *ctx) { return ctx->max; }
 
-size_t random_order_advance(random_order_t *ctx) { return ctx->state++; }
+size_t random_order_advance(random_order_t *ctx) {
+  size_t out = ctx->state;
+
+  // Normally, the increment is the step size. However, if we came back around
+  // to the start index, increase it by one to change the offset of the step.
+  uint32_t inc = ctx->step;
+  ctx->pos += inc;
+  if (ctx->pos >= ctx->max) {
+    inc++;
+    ctx->pos = 0;
+  }
+
+  if (launder32(inc) >= ctx->max - ctx->state) {
+    // If the next step would be greater than the maximum value, then loop back
+    // around to the start.
+    ctx->state = ctx->state + inc - ctx->max;
+  } else {
+    ctx->state += inc;
+  }
+  HARDENED_CHECK_LE(ctx->state, ctx->max);
+
+  return out;
+}

rtorok-zr/expo

sw/device/lib/base/random_order.c
@@ -5,15 +5,61 @@
 #include "sw/device/lib/base/random_order.h"
 
 #include "sw/device/lib/base/bitfield.h"
-
-// TODO: The current implementation is just a skeleton, and currently just
-// traverses from 0 to `min_len * 2`.
+#include "sw/device/lib/base/hardened.h"
 
 void random_order_init(random_order_t *ctx, size_t min_len) {
-  ctx->state = 0;
-  ctx->max = min_len * 2;
+  // Position relative to start index is always initialized to zero.
+  ctx->pos = 0;
+
+  // If the sequence length is zero or one, there's not much randomization we
+  // can do, but we can at least return one value past the range.
+  if (launder32(min_len) < 2) {
+    ctx->state = 0;
+    ctx->step = 1;
+    ctx->max = min_len + 1;
+    return;
+  }
+  HARDENED_CHECK_LE(2, min_len);
+
+  // Sample the start index from the range [0, n-1].
+  ctx->state = random_order_random_word() % min_len;
+  // Sample the step size from the range [1, n-1].
+  ctx->step = (random_order_random_word() % (min_len - 1)) + 1;
+
+  if (min_len - (min_len % ctx->step) > UINT32_MAX - ctx->step) {
+    // Safe fallback for cases where the max calculation would overflow.
+    ctx->max = UINT32_MAX;
+    ctx->step = 1;
+  } else {
+    // The maximum value is the smallest multiple of `step` that is strictly
+    // greater than `min_len`.
+    ctx->max = min_len - (min_len % ctx->step) + ctx->step;
+  }
+  HARDENED_CHECK_LE(min_len, ctx->max);
 }
 
 size_t random_order_len(const random_order_t *ctx) { return ctx->max; }
 
-size_t random_order_advance(random_order_t *ctx) { return ctx->state++; }
+size_t random_order_advance(random_order_t *ctx) {
+  size_t out = ctx->state;
+
+  // Normally, the increment is the step size. However, if we came back around
+  // to the start index, increase it by one to change the offset of the step.
+  uint32_t inc = ctx->step;
+  ctx->pos += inc;
+  if (ctx->pos >= ctx->max) {
+    inc++;
+    ctx->pos = 0;
+  }
+
+  if (launder32(inc) >= ctx->max - ctx->state) {
+    // If the next step would be greater than the maximum value, then loop back
+    // around to the start.
+    ctx->state = ctx->state + inc - ctx->max;
+  } else {
+    ctx->state += inc;
+  }
+  HARDENED_CHECK_LE(ctx->state, ctx->max);
+
+  return out;
+}