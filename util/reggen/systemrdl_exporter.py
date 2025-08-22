lowRISC/opentitan

[reggen, systemrdl] Add shadowed as a custom property (UDP)
Signed-off-by: Douglas Reis <doreis@lowrisc.org>

util/reggen/systemrdl_exporter.py
@@ -144,6 +144,9 @@ class BaseRegister2Systemrdl:
         rdl_t.external = self.inner.hwext
         if self.inner.hwre:
             self.importer.assign_property(rdl_t, "hwre", self.inner.hwre)
+
+        if self.inner.shadowed:
+            self.importer.assign_property(rdl_t, "shadowed", self.inner.shadowed)
         return rdl_t
 
 

rtorok-zr/expo

util/reggen/systemrdl_exporter.py
@@ -153,6 +153,9 @@ class Register2Systemrdl:
         if self.inner.hwre:
             self.importer.assign_property(reg_type, "hwre", self.inner.hwre)
 
+        if self.inner.shadowed:
+            self.importer.assign_property(reg_type, "shadowed", self.inner.shadowed)
+
         reg = self.importer.instantiate_reg(
             reg_type,
             self.inner.name,