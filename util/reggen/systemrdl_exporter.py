lowRISC/opentitan

[reggen, systemrdl] Add Hwre as a custom property (UDP)
Signed-off-by: Douglas Reis <doreis@lowrisc.org>

util/reggen/systemrdl_exporter.py
@@ -142,6 +142,8 @@ class BaseRegister2Systemrdl:
             self.importer.add_child(rdl_t, Field2Systemrdl(rfield, self.importer).export())
 
         rdl_t.external = self.inner.hwext
+        if self.inner.hwre:
+            self.importer.assign_property(rdl_t, "hwre", self.inner.hwre)
         return rdl_t
 
 

rtorok-zr/expo

util/reggen/systemrdl_exporter.py
@@ -150,6 +150,9 @@ class Register2Systemrdl:
 
         reg_type.external = self.inner.hwext
 
+        if self.inner.hwre:
+            self.importer.assign_property(reg_type, "hwre", self.inner.hwre)
+
         reg = self.importer.instantiate_reg(
             reg_type,
             self.inner.name,