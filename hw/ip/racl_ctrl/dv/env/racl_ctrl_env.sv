lowRISC/opentitan

[racl_ctrl,dv] Avoid some transactions with uvm_config_db
This should be a bit more efficient because we no longer have to
look up each interface in turn.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/racl_ctrl/dv/env/racl_ctrl_env.sv
@@ -21,27 +21,28 @@ function racl_ctrl_env::new (string name="", uvm_component parent=null);
 endfunction
 
 function void racl_ctrl_env::build_phase(uvm_phase phase);
+  racl_ctrl_env_wrapper wrapper;
+
   super.build_phase(phase);
 
-  if (!uvm_config_db#(int unsigned)::get(null, "*.env",
-                                         "num_subscribing_ips",
-                                         cfg.internal_error_agent_cfg.num_subscribing_ips))
-    `uvm_info(`gfn, "No num_subscribing_ips value in config db. Default to zero.", UVM_LOW)
+  // Consume the configuration for the environment from uvm_config_db
+  if (!uvm_config_db#(racl_ctrl_env_wrapper)::get(null, "*.env", "wrapper", wrapper)) begin
+    `uvm_fatal(`gfn, "Cannot find wrapper object in config db")
+  end
+  cfg.internal_error_agent_cfg.num_subscribing_ips = wrapper.num_subscribing_ips;
+  cfg.external_error_agent_cfg.num_subscribing_ips = wrapper.num_external_subscribing_ips;
 
-  if (!uvm_config_db#(int unsigned)::get(null, "*.env",
-                                         "num_external_subscribing_ips",
-                                         cfg.external_error_agent_cfg.num_subscribing_ips))
-    `uvm_info(`gfn, "No num_external_subscribing_ips value in config db. Default to zero.", UVM_LOW)
+  cfg.policies_vif                 = wrapper.policies_vif;
+  cfg.internal_error_agent_cfg.vif = wrapper.internal_error_vif;
+  cfg.external_error_agent_cfg.vif = wrapper.external_error_vif;
 
-  internal_error_agent = racl_error_log_agent::type_id::create("internal_error_agent", this);
+  // Connect up configuration for each of the agents in uvm_config_db
   uvm_config_db#(racl_error_log_agent_cfg)::set(this, "internal_error_agent*", "cfg",
                                                 cfg.internal_error_agent_cfg);
-
-  external_error_agent = racl_error_log_agent::type_id::create("external_error_agent", this);
   uvm_config_db#(racl_error_log_agent_cfg)::set(this, "external_error_agent*", "cfg",
                                                 cfg.external_error_agent_cfg);
 
-  if (!uvm_config_db#(virtual racl_ctrl_policies_if)::get(null, "*.env",
-                                                          "policies_if", cfg.policies_vif))
-    `uvm_fatal(`gfn, "failed to get policies_if handle from config db")
+  // Create tha agents
+  internal_error_agent = racl_error_log_agent::type_id::create("internal_error_agent", this);
+  external_error_agent = racl_error_log_agent::type_id::create("external_error_agent", this);
 endfunction

hw/ip/racl_ctrl/dv/env/racl_ctrl_env_pkg.sv
@@ -36,6 +36,7 @@ package racl_ctrl_env_pkg;
 
   // package sources
   `include "racl_ctrl_reg_window.sv"
+  `include "racl_ctrl_env_wrapper.sv"
   `include "racl_ctrl_env_cfg.sv"
   `include "racl_ctrl_env_cov.sv"
   `include "racl_ctrl_virtual_sequencer.sv"

rtorok-zr/expo

hw/ip/racl_ctrl/dv/env/racl_ctrl_env.sv
@@ -21,30 +21,28 @@ function racl_ctrl_env::new (string name="", uvm_component parent=null);
 endfunction
 
 function void racl_ctrl_env::build_phase(uvm_phase phase);
+  racl_ctrl_env_wrapper_cfg wrapper;
+
   super.build_phase(phase);
 
-  if (!uvm_config_db#(int unsigned)::get(null, "*.env",
-                                         "num_subscribing_ips",
-                                         cfg.internal_error_agent_cfg.num_subscribing_ips)) begin
-    `uvm_info(`gfn, "No num_subscribing_ips value in config db. Default to zero.", UVM_LOW)
+  // Consume the configuration for the environment from uvm_config_db
+  if (!uvm_config_db#(racl_ctrl_env_wrapper_cfg)::get(null, "*.env", "wrapper", wrapper)) begin
+    `uvm_fatal(`gfn, "Cannot find wrapper object in config db")
   end
+  cfg.internal_error_agent_cfg.num_subscribing_ips = wrapper.num_subscribing_ips;
+  cfg.external_error_agent_cfg.num_subscribing_ips = wrapper.num_external_subscribing_ips;
 
-  if (!uvm_config_db#(int unsigned)::get(null, "*.env",
-                                         "num_external_subscribing_ips",
-                                         cfg.external_error_agent_cfg.num_subscribing_ips)) begin
-    `uvm_info(`gfn, "No num_external_subscribing_ips value in config db. Default to zero.", UVM_LOW)
-  end
+  cfg.policies_vif                 = wrapper.policies_vif;
+  cfg.internal_error_agent_cfg.vif = wrapper.internal_error_vif;
+  cfg.external_error_agent_cfg.vif = wrapper.external_error_vif;
 
-  internal_error_agent = racl_error_log_agent::type_id::create("internal_error_agent", this);
+  // Connect up configuration for each of the agents in uvm_config_db
   uvm_config_db#(racl_error_log_agent_cfg)::set(this, "internal_error_agent*", "cfg",
                                                 cfg.internal_error_agent_cfg);
-
-  external_error_agent = racl_error_log_agent::type_id::create("external_error_agent", this);
   uvm_config_db#(racl_error_log_agent_cfg)::set(this, "external_error_agent*", "cfg",
                                                 cfg.external_error_agent_cfg);
 
-  if (!uvm_config_db#(virtual racl_ctrl_policies_if)::get(null, "*.env",
-                                                          "policies_if", cfg.policies_vif)) begin
-    `uvm_fatal(`gfn, "failed to get policies_if handle from config db")
-  end
+  // Create tha agents
+  internal_error_agent = racl_error_log_agent::type_id::create("internal_error_agent", this);
+  external_error_agent = racl_error_log_agent::type_id::create("external_error_agent", this);
 endfunction

hw/ip/racl_ctrl/dv/env/racl_ctrl_env_pkg.sv
@@ -36,6 +36,7 @@ package racl_ctrl_env_pkg;
 
   // package sources
   `include "racl_ctrl_reg_window.sv"
+  `include "racl_ctrl_env_wrapper_cfg.sv"
   `include "racl_ctrl_env_cfg.sv"
   `include "racl_ctrl_env_cov.sv"
   `include "racl_ctrl_virtual_sequencer.sv"