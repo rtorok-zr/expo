0f02868d6bb9cec10c89d6f47640a15e22acc19a
Kat Fox <kat@zerorisc.com>
2025-09-11 20:36:04 +0100

    [crypto] Run buildifier_fix and clang_format_fix to clean previous commits

--- sw/device/lib/crypto/impl/ed25519.c
+++ sw/device/lib/crypto/impl/ed25519.c
@@ -6,9 +6,10 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "sw/device/lib/crypto/impl/ecc/ed25519.h"
+
 #include "sw/device/lib/base/math.h"
 #include "sw/device/lib/crypto/drivers/entropy.h"
-#include "sw/device/lib/crypto/impl/ecc/ed25519.h"
 #include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
