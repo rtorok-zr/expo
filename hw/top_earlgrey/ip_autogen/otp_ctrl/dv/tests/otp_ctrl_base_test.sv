7bf94410bd901fbae2fd3010e7ec00025d6d3d1d
Ryan Torok <rtorok@zerorisc.com>
2025-10-28 21:34:04 +0000

    [license-checker] Repatriate license checker, allow different licenses
    Move the license checker back from lowRISC/misc-linters. The
    config file now contains multiple licenses, and the check passes
    if a file contains _any one_ of these licenses.
    
    Signed-off-by: Ryan Torok <rtorok@zerorisc.com>

--- hw/top_earlgrey/ip_autogen/otp_ctrl/dv/tests/otp_ctrl_base_test.sv
+++ hw/top_earlgrey/ip_autogen/otp_ctrl/dv/tests/otp_ctrl_base_test.sv
@@ -1,5 +1,5 @@
 // Copyright lowRISC contributors (OpenTitan project).
-// Copyright zeroRISC Inc
+// Copyright zeroRISC Inc.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
